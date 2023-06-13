2a8d8bfe48cbda22932e70f68a161af4d147a9b6
Mathu <z5419643@ad.unsw.edu.au>
2023-06-13 16:21:42 +1000

    Almost finished last qtn

--- wk3/scraping_courses.sh
+++ wk3/scraping_courses.sh
@@ -2,8 +2,8 @@
 
 
 
-URL1="https://www.handbook.unsw.edu.au/api/content/render/false/query/+unsw_psubject.implementationYear:${1}%20+unsw_psubject.studyLevel:undergraduate%20+unsw_psubject.educationalArea:COMP*%20+unsw_psubject.active:1%20+unsw_psubject.studyLevelValue:ugrd%20+deleted:false%20+working:true%20+live:true/orderby/unsw_psubject.code%20asc/limit/10000/offset/0"
-URL2="https://www.handbook.unsw.edu.au/api/content/render/false/query/+unsw_psubject.implementationYear:${1}%20+unsw_psubject.studyLevel:postgraduate%20+unsw_psubject.educationalArea:COMP*%20+unsw_psubject.active:1%20+unsw_psubject.studyLevelValue:pgrd%20+deleted:false%20+working:true%20+live:true/orderby/unsw_psubject.code%20asc/limit/10000/offset/0"
+URL1="https://www.handbook.unsw.edu.au/api/content/render/false/query/+unsw_psubject.implementationYear:${1}%20+unsw_psubject.studyLevel:undergraduate%20+unsw_psubject.educationalArea:${2}*%20+unsw_psubject.active:1%20+unsw_psubject.studyLevelValue:ugrd%20+deleted:false%20+working:true%20+live:true/orderby/unsw_psubject.code%20asc/limit/10000/offset/0"
+URL2="https://www.handbook.unsw.edu.au/api/content/render/false/query/+unsw_psubject.implementationYear:${1}%20+unsw_psubject.studyLevel:postgraduate%20+unsw_psubject.educationalArea:${2}*%20+unsw_psubject.active:1%20+unsw_psubject.studyLevelValue:pgrd%20+deleted:false%20+working:true%20+live:true/orderby/unsw_psubject.code%20asc/limit/10000/offset/0"
 
 if [ "$#" -ne 2 ]
 then
@@ -12,11 +12,15 @@ fi
 
 if [ "$1" -ge 2019 ] && [ "$1" -le 2023 ]
 then
-    arr=$(curl -sL "$URL1" | jq '[. | {code: .contentlets[].code, title: .contentlets[].title}]')
-    for code in $arr
-    do
-        echo "$code"
-    done
+    curl -sL "$URL1" | jq -rc '.contentlets[] | .code, .title' | sed -E ':a;N;$!ba;s/([0-9])\n/\1 /g'
+    # for code in $arr[]
+    # do
+    #     echo "$code"
+    # done
 else
     echo "$0: argument 1 must be an integer between 2019 and 2023"
 fi
+
+
+#grep -Ei ""code: $2"" | cut -d\: -f2 | sed "s/\",//g" | sed "s/\"//g"
+#. | {code: .contentlets[].code, title: .contentlets[].title}
