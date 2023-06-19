567002f74a052e13c06c6fb69d9f953ce34ecb4f
Mathu <z5419643@ad.unsw.edu.au>
2023-06-19 15:40:45 +1000

    finished q1

--- wk4/jpg2png.sh
+++ wk4/jpg2png.sh
@@ -1,6 +1,18 @@
 #!/bin/dash
-
+# set -x
 for file_name in *
 do
-    echo $file_name
+    test -f "$file_name" ||
+    continue
+    if echo "$file_name" | grep -E ".jpg$" >/dev/null
+    then
+    old_name="$(
+        echo $file_name |
+        cut -d\. -f1
+    )"
+    jpg_name=${old_name}.jpg
+    png_name=${old_name}.png
+    convert "$jpg_name" "$png_name"
+    rm "$jpg_name"
+    fi
 done
