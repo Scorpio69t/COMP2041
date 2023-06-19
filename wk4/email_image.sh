374c0b9af5154eca1d18866dc3265708ba87bfaf
Mathu <z5419643@ad.unsw.edu.au>
2023-06-19 16:30:29 +1000

    Working on mtut

---
+++ wk4/email_image.sh
@@ -0,0 +1,12 @@
+#!/bin/dash
+
+for image in "$@"
+do
+    display "$image"
+    echo -n "Address to e-mail this image to? "
+    read email
+    echo -n "Message to accompany image? "
+    read message
+    echo "$message" | mutt -s 'SUBJECT?' -e 'set copy=no' -a "$image" -- "$email"
+    echo "$image sent to $email"
+done
