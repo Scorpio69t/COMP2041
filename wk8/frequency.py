eddf3660f98d7b74feaf7f2227e2211171c64c90
Mathu <z5419643@ad.unsw.edu.au>
2023-07-17 09:45:33 +1000

    Finished q3

---
+++ wk8/frequency.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import re, sys, glob
+totalSum = 0
+wordSum = 0
+for file in sorted(glob.glob("lyrics/*.txt")):
+    fileName = file.split("/")[1]
+    fileName = fileName.split(".")[0]
+    fileName = fileName.replace("_", " ")
+    with open(file) as lyricFile:
+        for line in lyricFile:
+                m = re.split('[^a-zA-Z]', line)
+                m = list(filter(None, m))
+                m = [x.lower() for x in m]
+                wordSum += m.count(sys.argv[1])
+                totalSum += len(m)
+    print(f"{wordSum:4}/{totalSum:6} = {wordSum/totalSum:.9f} {fileName}")
