2b30a9c24cde5ed0696a743a063e97f86efa137d
Mathu <z5419643@ad.unsw.edu.au>
2023-07-24 09:52:15 +1000

    Added uniq

--- wk9/courses.sh
+++ wk9/courses.sh
@@ -1,4 +1,4 @@
 #!/bin/dash
 
 curl --location --silent "http://www.timetable.unsw.edu.au/2023/{$1}KENS.html" | grep -E "<td class=.*><a href=.*>.*</a></td>$" | grep -Ev "<td class=.*><a href=.*>[A-Z]{4}[0-9]{4}</a></td>$" |
-cut -d"=" -f3 | cut -d"<" -f1 | sed -E "s/>/ /g" | sed -E "s/\"//g" | sed -E "s/.html//g" | tail -n +6 | sort -k1n
+cut -d"=" -f3 | cut -d"<" -f1 | sed -E "s/>/ /g" | sed -E "s/\"//g" | sed -E "s/.html//g" | tail -n +6 | sort -k1n | uniq
