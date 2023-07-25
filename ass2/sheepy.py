bee1f335eec4a555cabaa830bc411dbb26a35be9
Mathu <z5419643@ad.unsw.edu.au>
2023-07-25 20:11:35 +1000

    finished subset0

--- ass2/sheepy.py
+++ ass2/sheepy.py
@@ -13,16 +13,13 @@ def comment_split(line):
     return line.split('#')
 
 #MAKE THIS A FUNCTION THAT CHANGES ECHO TO PRINT. NOTHING ELSE GIT
-def shell_interpret(line):
-    #If line is an echo including variables
-    #if line is an echo
+def echo_expand(line):
+    #if line is an echo, print it
     if m := re.search("^echo\s+(.*)$", str(line)):
-        print(m.group(1))
-        line = line.replace("echo ", f"print(f\"{' '.join(m.group(1).split())}\")")
-        print(line)
-        # print("print(f\"", end="")
-        # print(" ".join(m.group(1).split()), end="")
-        # print("\")")
+        line = line.replace(line, f"print(f\"{' '.join(m.group(1).split())}\")")
+        print(line, end="")
+
+def variable_assignment(line):
     #If line is a variable
     if m:= re.search("(\w+)=(.*)", line):
         print(f"{m.group(1)} = \"{m.group(2)}\"")
@@ -40,18 +37,25 @@ def main():
     #   if keywords realting to modules are in shell list, print import line.
     #########
 
-    print("#!/usr/bin/python3 -u\n")
+    print("#!/usr/bin/python3 -u")
 
     with open(sys.argv[1]) as f:
+        next(f)
         for line in f:
-            line = variable_replace(line)
-            line = comment_split(line)
-            shell_interpret(line)
+            comment = None                              #Set comment to none for each line, so that comment does not carry on to next line.
+            variable_assignment(line)   
+            line = variable_replace(line)               # Basic functions that assign variables and replace
 
+            if re.search("#", line):
+                comment = comment_split(line)[1]        #If comment exists, comment becomes the comment string.
 
+            line = comment_split(line)[0]               #At this point, line does not include the comment
+            echo_expand(line)                       #Print the line. Print comment aswell if it exists, else a newline.
+
+            if comment:
+                print(f" #{comment}")
+            else:
+                print("")
 if __name__ == "__main__":
     main()
 
-'''
-for x in "$list"
-'''
--- ass2/tmp.py
+++ ass2/tmp.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3 -u
 
 a = "hello"
+
 b = "world"
+
+
 print(f"{a} {b}")
